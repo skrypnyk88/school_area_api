8905c38fc927f13f3bf4ac07547127f3bf1c33c3
Viktoriia Bilous <vikabelous@gmail.com>
2017-03-31 21:08:04 +0300

    LVRUBYM-271: Add Group model

---
+++ app/models/group.rb
@@ -0,0 +1,17 @@
+class Group < ApplicationRecord
+  before_save :capitalize_name
+
+  validates :name,
+            presence: true,
+            length: { maximum: 20 },
+            format: {
+              with: /\A[a-z0-9 -]+\z/i,
+              message: 'allows only letters or numbers'
+            }
+
+  private
+
+  def capitalize_name
+    self.name = name.capitalize
+  end
+end
