5e62944c17435f4e19ad9889a360cdc9d86d6bd3
Mykhailo Shtohryn <mshtogryn@gmail.com>
2017-03-31 17:23:41 +0300

    LVRUBYM-219: Add comments

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,16 +1,34 @@
 Rails.application.configure do
+  # The test environment is used exclusively to run your application's
+  # test suite. You never need to work with it otherwise. Remember that
+  # your test database is "scratch space" for the test suite and is wiped
+  # and recreated between test runs. Don't rely on the data there!
   config.cache_classes = true
+
+  # If you are using a tool that preloads Rails for running tests,
+  # you may have to set it to true.
   config.eager_load = false
+
+  # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
     'Cache-Control' => 'public, max-age=3600'
   }
 
+  # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
+
+  # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
+
+  # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
   config.action_mailer.perform_caching = false
+
+  # Tell Action Mailer not to deliver emails to the real world.
   config.action_mailer.delivery_method = :test
+
+  # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 end
--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -1,4 +1,9 @@
+# Make Ruby 2.4 preserve the timezone of the receiver when calling `to_time`.
 ActiveSupport.to_time_preserves_timezone = true
+
+# Require `belongs_to` associations by default. Previous versions had false.
 Rails.application.config.active_record.belongs_to_required_by_default = true
+
+# Do not halt callback chains when a callback returns false.
 ActiveSupport.halt_callback_chains_on_return_false = false
 Rails.application.config.ssl_options = { hsts: { subdomains: true } }
