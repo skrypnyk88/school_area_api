79b0d624a6ed140a88198957d371f146241a627c
Vitalia <vitalia.durnyak@gmail.com>
2017-04-01 15:39:08 +0300

    LVRUBYM-233:Modified seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,3 +26,20 @@ group = Group.create(name: 'Group 1')
     student_id: student.id
   )
 end
+
+FactoryGirl.define do
+  factory :bottle_report do
+    day { Faker::Date.forward(23) }
+  end
+
+  factory :bottle do
+    bottle_report
+    quantity { Faker::Number.between(30,300) }
+    time '15:03'
+    uom { %w(ml oz).sample }
+
+  end
+end
+
+10.times { FactoryGirl.create(:bottle_report) }
+10.times { FactoryGirl.create(:bottle) }
