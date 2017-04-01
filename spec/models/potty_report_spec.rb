683585104c61db83552936da4868d1d2210c00f3
hmaherovska <maher.olena96@gmail.com>
2017-04-01 13:28:31 +0300

    LVRUBYM-251: create model PottyReport

--- spec/models/potty_report_spec.rb
+++ spec/models/potty_report_spec.rb
@@ -1,17 +1,16 @@
 require 'rails helper'
 
 RSpec.describe PottyReport , type: :model do
-	describe '#day' do
-		context 'when day is not blank' do
-			it 'is valid' do
-				expect(build(:potty_report, day: "Mon,25 Sep 2017").to be_valid
-			end
+ describe '#day' do
+	context 'when day is not blank' do
+	  it 'is valid' do
+		expect(build(:potty_report, day: "Mon,25 Sep 2017").to be_valid
+	  end
+	end
+	context 'when day is blank' do
+		it 'is not valid' do
+			expect(build(:potty_report, day: "").to_not be_valid
 		end
-
-		context 'when day is blank' do
-			it 'is not valid' do
-				expect(build(:potty_report, day: "").to_not be_valid
-			end
-		end
+	end
 	end
 end
