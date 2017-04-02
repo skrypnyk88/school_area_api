f9097f288e685044b705cf9038add2c72e04f604
Mykhailo Shtohryn <mshtogryn@gmail.com>
2017-04-02 15:47:10 +0300

    LVRUBYM-193: PresentReport model creation

--- db/migrate/20170402122940_create_report_times.rb
+++ db/migrate/20170402122940_create_report_times.rb
@@ -3,7 +3,6 @@ class CreateReportTimes < ActiveRecord::Migration[5.0]
     create_table :report_times do |t|
       t.datetime :start_time
       t.datetime :end_time
-<<<<<<< b65e4a29b844cb164727aae68d6b9ad169158d53
       t.integer  :reportable_id
       t.string   :reportable_type
       t.integer  :created_by
@@ -12,10 +11,5 @@ class CreateReportTimes < ActiveRecord::Migration[5.0]
       t.timestamps
     end
     add_index :report_times, [:reportable_type, :reportable_id]
-=======
-      
-      t.timestamps
-    end
->>>>>>> LVRUBYM-203: ReportTime model creation
   end
 end
--- db/migrate/20170402123802_create_presence_reports.rb
+++ db/migrate/20170402123802_create_presence_reports.rb
@@ -1,7 +1,6 @@
 class CreatePresenceReports < ActiveRecord::Migration[5.0]
   def change
     create_table :presence_reports do |t|
-<<<<<<< b65e4a29b844cb164727aae68d6b9ad169158d53
       t.date     :day
       t.integer  :updated_by
 
@@ -9,10 +8,5 @@ class CreatePresenceReports < ActiveRecord::Migration[5.0]
     end
     add_reference :presence_reports, :group, index: true, foreign_key: true
     add_reference :presence_reports, :student, index: true, foreign_key: true
-=======
-
-      t.timestamps
-    end
->>>>>>> LVRUBYM-203: ReportTime model creation
   end
 end
