8067f7d006af68dac94d6d6c6c3cf38f137fa2c8
hmaherovska <maher.olena96@gmail.com>
2017-04-03 23:31:15 +0300

    LVRUBYM-288: Removed attr_accessor

--- app/models/potty.rb
+++ app/models/potty.rb
@@ -1,5 +1,5 @@
 class Potty < ApplicationRecord
-  enum potty_type: { wet_diaper: 0, bm_diaper: 1, 
+  enum potty_type: { wet_diaper: 0, bm_diaper: 1,
                      wet_potty: 2, bm_potty: 3, dry: 4 }
   validates :time, :potty_type, presence: true
   validates_inclusion_of :potty_type, in: potty_types.keys
