b5370e47963d30c50bf0efbd1b6d1ae7796fbca2
Viktoriia Bilous <vikabelous@gmail.com>
2017-04-04 16:29:19 +0300

    LVRUBYM-293: Change controller specs

--- spec/controllers/v1/students_controller_spec.rb
+++ spec/controllers/v1/students_controller_spec.rb
@@ -3,75 +3,60 @@ require 'rails_helper'
 RSpec.describe V1::StudentsController, type: :controller do
   render_views
 
-  let(:post_valid_student) do
-    post :create,
-         format: :json,
-         params: { student: attributes_for(:student) }
-  end
-
-  let(:post_invalid_student) do
-    post :create,
-         format: :json,
-         params: { student: attributes_for(:student, first_name: '111') }
-  end
-
-  let(:update_valid_student) do
-    post :update,
-         format: :json,
-         params: {
-           method: :patch,
-           id: @student,
-           student: { first_name: 'Aaaa' }
-         }
-  end
-
-  let(:update_invalid_student) do
-    post :update,
-         format: :json,
-         params: {
-           method: :patch,
-           id: @student,
-           student: { first_name: '111' }
-         }
-  end
-
-  let(:delete_student) do
-    post :destroy,
-         format: :json,
-         params: {
-           method: :delete,
-           id: @student
-         }
+  def student_json(student)
+    {
+      id: student.id,
+      first_name: student.first_name,
+      last_name: student.last_name,
+      birthdate: student.birthdate,
+      age: student.age,
+      gender: student.gender
+    }.to_json
   end
 
   before do
-    @student = create(:student)
+    @group = create(:group)
+    @student = create(:student, group: @group)
   end
 
   describe 'GET #index' do
     it 'returns all students' do
-      another_student = create(:student)
+      another_student = create(:student, group: @group)
 
-      get :index, format: :json
-      json = JSON.parse(response.body)
+      get :index, format: :json,
+                  params: { group_id: @group }
 
-      expect(json.any? { |hash| hash['id'] ==  @student.id }).to be true
-      expect(json.any? { |hash| hash['id'] ==  another_student.id }).to be true
+      expect(response.body).to include(student_json(@student))
+      expect(response.body).to include(student_json(another_student))
     end
   end
 
   describe 'POST #create' do
     context 'when student is valid' do
       it 'renders student json' do
-        post_valid_student
-        json = JSON.parse(response.body)
-        expect(json['id']).to eq(Student.last.id)
+        student_attrs = attributes_for(:student).with_indifferent_access
+        student_attrs[:birthdate] = student_attrs[:birthdate].to_s
+
+        post :create,
+             format: :json,
+             params: {
+               group_id: @group,
+               student: student_attrs
+             }
+
+        expect(JSON.parse(response.body)).to include(student_attrs)
       end
     end
 
     context 'when student is not valid' do
       it 'renders bad_request response' do
-        post_invalid_student
+        post :create,
+             format: :json,
+             params: {
+               group_id: @group,
+               student: attributes_for(:student, first_name: '111')
+             }
+
         expect(response).to have_http_status(:bad_request)
       end
     end
@@ -82,43 +67,61 @@ RSpec.describe V1::StudentsController, type: :controller do
       get :show, params: { id: @student },
                  format: :json
 
-      json = JSON.parse(response.body)
-
-      expect(json['id']).to eq(@student.id)
+      expect(response.body).to eq(student_json(@student))
     end
   end
 
   describe 'PATCH #update' do
     context 'when student is valid' do
+      before do
+        post :update, format: :json,
+                      params: {
+                        method: :patch,
+                        id: @student,
+                        student: { first_name: 'Aaaa' }
+                      }
+      end
+
       it 'updates students attributes' do
-        update_valid_student
         expect(@student.reload.first_name).to eq('Aaaa')
       end
 
       it 'renders student json' do
-        update_valid_student
-        json = JSON.parse(response.body)
-        expect(json['id']).to eq(@student.id)
+        expect(response.body).to eq(student_json(@student.reload))
       end
     end
 
     context 'when student is not valid' do
       it 'renders bad_request response' do
-        update_invalid_student
+        post :update,
+             format: :json,
+             params: {
+               method: :patch,
+               id: @student,
+               student: { first_name: '111' }
+             }
+
         expect(response).to have_http_status(:bad_request)
       end
     end
   end
 
   describe 'DELETE #destroy' do
+    before do
+      post :destroy,
+           format: :json,
+           params: {
+             method: :delete,
+             id: @student
+           }
+    end
+
     it 'deletes student' do
-      delete_student
       expect(Student.exists?(@student.id)).to be false
     end
 
     it 'renders ok response' do
-      delete_student
-      expect(response).to have_http_status(:ok)
+      expect(response).to have_http_status(:no_content)
     end
   end
 end
--- spec/factories/students.rb
+++ spec/factories/students.rb
@@ -1,8 +1,9 @@
 FactoryGirl.define do
   factory :student do
-    first_name { Faker::Name.first_name }
-    last_name { Faker::Name.last_name }
+    first_name { Faker::Name.first_name.titleize }
+    last_name { Faker::Name.last_name.titleize }
     birthdate { Faker::Date.birthday(2, 6) }
     gender { Faker::Demographic.sex.downcase }
+    group
   end
 end
