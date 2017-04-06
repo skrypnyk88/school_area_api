f7a6b9d0bfad347f25a6baabd68651e368baac0e
Dmytro <skrypnyk_d@ukr.net>
2017-04-06 10:34:50 +0300

    LVRUBYM-220: Save before --rebace

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,4 @@
-
-ActiveRecord::Schema.define(version: 20170403084317) do
+ActiveRecord::Schema.define(version: 20170331162525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,8 +36,6 @@ ActiveRecord::Schema.define(version: 20170403084317) do
     t.integer  "gender"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "group_id"
-    t.index ["group_id"], name: "index_students_on_group_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
