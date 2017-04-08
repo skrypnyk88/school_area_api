5f785311eebb7f254df4e0b2710327727267fdb9
Viktoriia Bilous <vikabelous@gmail.com>
2017-04-08 13:28:34 +0300

    LVRUBYM-218: Add JWT strategy for devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,7 +9,7 @@ Devise.setup do |config|
   # Configure which authentication keys should have whitespace stripped.
   config.strip_whitespace_keys = [:email]
   # skip storage for particular strategies by setting this option.
-  config.skip_session_storage = [:http_auth]
+  config.skip_session_storage = [:http_auth, :jwt]
   # ==> Configuration for :database_authenticatable
   # For bcrypt, this is the cost for hashing the password and defaults to 11
   config.stretches = Rails.env.test? ? 1 : 11
@@ -29,4 +29,44 @@ Devise.setup do |config|
   # ==> Navigation configuration
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
+
+  config.warden do |manager|
+    manager.strategies.add :jwt, Devise::Strategies::JWT
+    manager.default_strategies(scope: :user).unshift :jwt
+    manager.failure_app = JwtFailureApp
+  end
+end
+
+module Devise
+  module Strategies
+    class JWT < Base
+      def valid?
+        request.authorization
+      end
+
+      def authenticate!
+        payload = JsonWebToken.decode(token)
+        success! User.find(payload[:user_id])
+      rescue ::JWT::ExpiredSignature
+        fail! 'Auth token has expired'
+      rescue ::JWT::DecodeError
+        fail! 'Auth token is invalid'
+      end
+
+      private
+
+      def token
+        request.authorization.split.last
+      end
+    end
+  end
+end
+
+class JwtFailureApp < Devise::FailureApp
+  def respond
+    self.status = :unauthorized
+    self.content_type = 'application/json'
+    self.response_body = { error: request.env['warden.options'][:message] }
+                         .to_json
+  end
 end
