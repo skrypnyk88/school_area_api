4f967c6c6cb0b5b5031466afc9c04c1e44918333
Vitalia <vitalia.durnyak@gmail.com>
2017-04-11 11:54:45 +0300

    LVRUBYM-221:Fixed rspec tests with rubocop

--- spec/controllers/v1/bottle_reports_controller_spec.rb
+++ spec/controllers/v1/bottle_reports_controller_spec.rb
@@ -4,19 +4,15 @@ RSpec.describe V1::BottleReportsController, type: :controller do
   render_views
 
   def bottle_report_json(bottle_report)
-    { 'id' => bottle_report.id,
-      'day' => bottle_report.day.to_s,
+    { 'id' => bottle_report.id, 'day' => bottle_report.day.to_s,
       'group_id' => bottle_report.group_id,
       'student_id' => bottle_report.student_id,
       'bottles' => bottle_report.bottles
-                                .map { |b|
-                                  { 'id' => b.id,
-                                    'quantity' => b.quantity,
-                                    'time' => b.time.to_s,
-                                    'uom' => b.uom,
+                                .map do |b|
+                                  { 'id' => b.id, 'quantity' => b.quantity,
+                                    'time' => b.time.to_s, 'uom' => b.uom,
                                     'bottle_report_id' => b.bottle_report_id }
-                                }
-      }
+                                end }
   end
 
   let(:test_group) { FactoryGirl.create(:group) }
@@ -29,26 +25,28 @@ RSpec.describe V1::BottleReportsController, type: :controller do
     context 'when search for all bottle reports by group' do
       it 'return all bottle reports for exact group' do
         test_bottle_reports = FactoryGirl.create_list(:bottle_report, 10,
-                                                       group_id: test_group.id)
+                                                      group_id: test_group.id)
         get :index, format: :json,
                     group_id: test_group.id
 
         expect(JSON.parse(response.body))
-          .to eq(test_bottle_reports.collect { |entry| bottle_report_json(entry) })
+          .to eq(test_bottle_reports
+          .collect { |entry| bottle_report_json(entry) })
       end
     end
 
     context 'when search for all bottle reports by day and group' do
       it 'return all bottle reports for exact day and group' do
         test_bottle_reports = FactoryGirl.create_list(:bottle_report, 10,
-                                                       group_id: test_group.id,
-                                                       day: DateTime.now)
+                                                      group_id: test_group.id,
+                                                      day: DateTime.now)
         get :index, format: :json,
                     group_id: test_group.id,
                     day: DateTime.now
 
         expect(JSON.parse(response.body))
-          .to eq(test_bottle_reports.collect { |entry| bottle_report_json(entry) })
+          .to eq(test_bottle_reports
+          .collect { |entry| bottle_report_json(entry) })
       end
     end
   end
