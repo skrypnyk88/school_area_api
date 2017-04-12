2ecfef4becd49529066d9d6e271363c49da01b5b
Vitalia <vitalia.durnyak@gmail.com>
2017-04-13 01:05:20 +0300

    LVRUBYM-221:CI fixing

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,6 @@ Rails.application.routes.draw do
     resources :groups do
       resources :students, shallow: true
       resources :my_day_reports, shallow: true
-      resources :bottle_reports, param: :day, only: [:index, :show] do
-      resources :bottles, shallow: true
-    end
-    delete '/bottle_reports/:id', to: 'bottle_reports#destroy', as: 'destroy'
       resources :bottle_reports, only: [:index] do
         resources :bottles
       end
