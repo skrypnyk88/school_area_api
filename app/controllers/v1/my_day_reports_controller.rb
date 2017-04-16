5f38d5caf5f9549f67d397cbb1f7e41a1c234d5b
andriyarterchuk <andriyarterchuk@gmail.com>
2017-04-16 23:23:35 +0300

    LVRUBYM-224:deleted action create

--- app/controllers/v1/my_day_reports_controller.rb
+++ app/controllers/v1/my_day_reports_controller.rb
@@ -5,16 +5,11 @@ module V1
       @students = Group.find(params[:group_id]).students
     end
 
-    def create
-      @report = MyDayReport.new(report_params)
-      render_json_or_exception(@report.save, :create)
-    end
-
     def update
       @report = MyDayReport.find_by(id: params[:id],
                                     group_id: params[:group_id])
       head :not_found unless @report
-      render_json_or_exception(@report.update(report_params), :update)
+      @report.update(report_params) ? render :update :(head :bad_request)
     end
 
     private
@@ -23,12 +18,5 @@ module V1
       params.require(:report).permit(:day, :note)
     end
 
-    def find_students
-      @students = Group.find(params[:group_id]).students
-    end
-
-    def render_json_or_exception(condition, json_file)
-      condition ? (render json_file) : (head :bad_request)
-    end
   end
 end
