ed85f7b27b88c84a82ba264e68315ea62199548a
Vitalia <vitalia.durnyak@gmail.com>
2017-04-25 15:27:27 +0300

    LVRUBYM-333:Added teacher and groups seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,35 @@
 require 'faker'
 
-Teacher.create(
-  first_name: Faker::Name.first_name,
-  last_name: Faker::Name.last_name,
-  email: 'test@test.com',
-  password: 'password',
-  password_confirmation: 'password',
-  gender: %w(male female other).sample,
-  phone: '1234567890'
-)
+n = 0
 
-group = Group.create(name: 'Group 1')
-
-10.times do
-  student = group.students.create(
+2.times do |i|
+  teacher = Teacher.create(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
-    birthdate: Faker::Date.birthday(2, 6),
-    gender: Faker::Demographic.sex.downcase
+    email: "teacher#{i + 1}@test.com",
+    password: 'password',
+    password_confirmation: 'password',
+    gender: %w(male female other).sample,
+    phone: '1234567890'
   )
 
-  group.my_day_reports.create(
-    day: Faker::Date.backward(111),
-    note:  Faker::Lorem.sentence,
-    student_id: student.id
-  )
+  3.times do |j|
+    group = Group.create(name: "Group #{ n+=1 }")
+    teacher.groups << group
+
+    10.times do
+      student = group.students.create(
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        birthdate: Faker::Date.birthday(2, 6),
+        gender: Faker::Demographic.sex.downcase
+      )
+
+      group.my_day_reports.create(
+        day: Faker::Date.backward(111),
+        note:  Faker::Lorem.sentence,
+        student_id: student.id
+      )
+    end
+  end
 end
