bc552634694da34e60a027f1b4661b8a9af3547c
Viktoriia Bilous <vikabelous@gmail.com>
2017-05-09 10:53:07 +0300

    LVRUBYM-346: Update student controller tests and factories

--- spec/controllers/v1/students_controller_spec.rb
+++ spec/controllers/v1/students_controller_spec.rb
@@ -4,38 +4,46 @@ RSpec.describe V1::StudentsController, type: :controller do
   render_views
 
   def student_params(student)
-    student.attributes
-           .with_indifferent_access
-           .extract!(:id, :first_name, :last_name, :birthdate, :gender)
-           .update(age: student.age)
+    params = student.attributes
+                    .with_indifferent_access
+                    .extract!(:id, :first_name, :last_name, :birthdate, :gender)
+                    .merge(age: student.age)
+
+    if student.attachment
+      params.merge(url: "http://#{request.host}#{student.attachment.file.url}")
+    else
+      params
+    end
   end
 
-  let(:current_user) { create(:user) }
+  let(:teacher) { create(:teacher) }
 
   let(:group) { create(:group) }
 
-  let(:student) { create(:student, group: group) }
+  let(:current_user_group) { create(:group_with_students) }
+
+  let(:student) { current_user_group.students.first }
 
   let(:student_attrs) { attributes_for(:student).with_indifferent_access }
 
-  let(:group_with_students) do
-    create_list(:student, 3, group: group)
-    group
-  end
-
   before do
     allow(controller).to receive(:authenticate_user!)
-    allow(subject).to receive(:current_user).and_return(current_user)
+    allow(controller).to receive(:current_user).and_return(teacher)
+    teacher.groups << current_user_group
   end
 
   describe 'GET #index' do
+    before do
+      create(:group_with_students)
+    end
+
     it 'returns all students' do
-      students = group_with_students.students
-                                    .map { |s| student_params(s) }
-                                    .to_json
+      students = current_user_group.students
+                                   .map { |s| student_params(s) }
+                                   .to_json
 
       get :index, format: :json,
-                  params: { group_id: group_with_students }
+                  params: { group_id: current_user_group }
 
       expect(response.body).to eq(students)
     end
@@ -47,7 +55,7 @@ RSpec.describe V1::StudentsController, type: :controller do
         post :create,
              format: :json,
              params: {
-               group_id: group,
+               group_id: current_user_group,
                student: student_attrs
              }
 
@@ -60,7 +68,7 @@ RSpec.describe V1::StudentsController, type: :controller do
         post :create,
              format: :json,
              params: {
-               group_id: group,
+               group_id: current_user_group,
                student: student_attrs
              }
 
@@ -69,23 +77,31 @@ RSpec.describe V1::StudentsController, type: :controller do
     end
 
     context 'when student is not valid' do
-      it 'renders bad_request response' do
+      let(:invalid_student) { build(:student, first_name: '111') }
+
+      it 'renders errors json' do
+        allow(Student).to receive(:new).and_return(invalid_student)
+
         post :create,
              format: :json,
              params: {
-               group_id: group,
-               student: attributes_for(:student, first_name: '111')
+               group_id: current_user_group,
+               student: invalid_student.attributes
              }
 
-        expect(response).to have_http_status(:bad_request)
+        expect(response.body)
+          .to eq(invalid_student.errors.full_messages.to_json)
       end
     end
   end
 
   describe 'GET #show' do
     it 'renders student json' do
-      get :show, params: { id: student },
-                 format: :json
+      get :show, format: :json,
+                 params: {
+                   group_id: current_user_group,
+                   id: student
+                 }
 
       expect(response.body).to eq(student_params(student).to_json)
     end
@@ -95,6 +111,7 @@ RSpec.describe V1::StudentsController, type: :controller do
     context 'when student is valid' do
       let(:valid_params) do
         { method: :patch,
+          group_id: current_user_group,
           id: student,
           student: { first_name: 'Aaaa' } }
       end
@@ -118,22 +135,32 @@ RSpec.describe V1::StudentsController, type: :controller do
     context 'when student is not valid' do
       let(:invalid_params) do
         { method: :patch,
+          group_id: current_user_group,
           id: student,
           student: { first_name: '111' } }
       end
 
-      it 'renders bad_request response' do
+      let(:update_errors) do
+        student.update_attributes(first_name: '111')
+        student.errors.full_messages
+      end
+
+      it 'renders errors json' do
         post :update,
              format: :json,
              params: invalid_params
 
-        expect(response).to have_http_status(:bad_request)
+        expect(response.body).to eq(update_errors.to_json)
       end
     end
   end
 
   describe 'DELETE #destroy' do
-    let(:delete_params) { { method: :delete, id: student } }
+    let(:delete_params) do
+      { method: :delete,
+        group_id: current_user_group,
+        id: student }
+    end
 
     it 'deletes student' do
       post :destroy,
@@ -149,6 +176,49 @@ RSpec.describe V1::StudentsController, type: :controller do
            params: delete_params
 
       expect(response).to have_http_status(:no_content)
+    end
+  end
+
+  describe 'PUT #upload' do
+    let(:valid_attachment) { create(:attachment, attachable: student) }
+
+    let(:invalid_attachment) { build(:attachment, file: nil) }
+
+    let(:params) do
+      { method: :put,
+        group_id: current_user_group,
+        id: student,
+        file: {
+          filename: '',
+          filetype: '',
+          base64: ''
+        } }
+    end
+
+    context 'when attachment is valid' do
+      it 'renders student json' do
+        allow_any_instance_of(SingleAttachmentUploader)
+          .to receive(:call)
+          .and_return(valid_attachment)
+
+        post :upload, format: :json, params: params
+
+        expect(response.body).to eq(student_params(student).to_json)
+      end
+    end
+
+    context 'when attachment is not valid' do
+      it 'renders errors json' do
+        allow_any_instance_of(SingleAttachmentUploader)
+          .to receive(:call)
+          .and_return(invalid_attachment)
+
+        post :upload, format: :json,
+                      params: params
+
+        expect(response.body)
+          .to eq(invalid_attachment.errors.full_messages.to_json)
+      end
     end
   end
 end
