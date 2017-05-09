bba25173445cab7033294fbc97d24076fccd9b09
Julia Shtohryn <julia.ilkiv@gmail.com>
2017-05-09 19:23:28 +0300

    LVRUBYM-300: Added translates for mailer

--- app/mailers/reset_password_mailer.rb
+++ app/mailers/reset_password_mailer.rb
@@ -3,6 +3,7 @@ class ResetPasswordMailer < ApplicationMailer
   default template_path: 'v1/users/mailer'
 
   def reset_password_instructions(user)
+    I18n.locale = user.locale
     @user = user
     mail(to: @user.email,
          subject: I18n.t('mailer.reset_password_instructions_subject'))
