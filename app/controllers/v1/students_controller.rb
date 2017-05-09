dc133f98abd27de56f1a1c79df96df42c9ba4f02
Viktoriia Bilous <vikabelous@gmail.com>
2017-05-09 10:52:08 +0300

    LVRUBYM-346: Add upload action

--- app/controllers/v1/students_controller.rb
+++ app/controllers/v1/students_controller.rb
@@ -1,20 +1,31 @@
 module V1
   class StudentsController < ApplicationController
-    before_action :find_student, only: [:show, :update, :destroy]
+    include Attachable
+    include Groupable
+
+    before_action :find_student, except: [:index, :create]
 
     def index
-      @students = Group.find(params[:group_id]).students
+      @students = Student.where(group: @group).includes(:attachment)
     end
 
     def create
       @student = Student.new(student_params)
-      render_show_or_bad_request(@student.save)
+      if @student.save
+        render :show, status: :created
+      else
+        render json: @student.errors.full_messages, status: :bad_request
+      end
     end
 
     def show; end
 
     def update
-      render_show_or_bad_request(@student.update(student_params))
+      if @student.update_attributes(student_params)
+        render :show
+      else
+        render json: @student.errors.full_messages, status: :bad_request
+      end
     end
 
     def destroy
@@ -22,20 +33,22 @@ module V1
       head :no_content
     end
 
+    def upload
+      attachment = attachment_uploader.call(attachment_owner: @student)
+      if attachment.valid?
+        render :show, status: :created
+      else
+        render json: attachment.errors.full_messages, status: :bad_request
+      end
+    end
+
     private
 
     def find_student
-      @student = Student.find_by(id: params[:id])
-      head :not_found unless @student
-    end
+      @student = Student.find_by(id: params[:id], group: @group)
+      return if @student
 
-    def render_show_or_bad_request(succeed)
-      if succeed
-        render :show
-      else
-        render json: { errors: @student.errors.full_messages },
-               status: :bad_request
-      end
+      render json: 'Student not found'.to_json, status: :not_found
     end
 
     def student_params
