e2cd801ab80f1532229089d308ed486b831b77f3
Dmytro <skrypnyk_d@ukr.net>
2017-05-10 22:51:29 +0300

    LVRUBYM-220:Add rspec tests

--- app/models/health_report.rb
+++ app/models/health_report.rb
@@ -1,4 +1,8 @@
 class HealthReport < ApplicationRecord
+  belongs_to :group
+  belongs_to :student
+
   validates :health_note, length: { maximum: 250 }
-  validates :special_care, inclusion: { in: [ true, false ] }
+  validates :special_care, inclusion: { in: [true, false] }
+  validates :day, presence: true
 end
