503c08a28692d52ccad5541fa85184d0b078c0ee
Viktoriia Bilous <vikabelous@gmail.com>
2017-05-13 11:41:32 +0300

    LVRUBYM-359: Add error messages for authentication

--- lib/devise/jwt_failure_app.rb
+++ lib/devise/jwt_failure_app.rb
@@ -3,6 +3,7 @@ module Devise
     def respond
       self.status = :authentication_timeout
       self.content_type = 'application/json'
+      self.response_body = { errors: [I18n.t('errors.token_timeout')] }.to_json
     end
   end
 end
