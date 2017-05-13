cee35ebb6224a3028f2480924718af2ae38d68e8
Vita <vitalia.durnyak@gmail.com>
2017-05-13 16:17:19 +0300

    LVRUBYM-221:Removed groupable module from bottle report controller

--- app/controllers/v1/bottle_reports_controller.rb
+++ app/controllers/v1/bottle_reports_controller.rb
@@ -1,7 +1,6 @@
 module V1
   class BottleReportsController < ApplicationController
     include Reportable
-    include Groupable
 
     def index
       @bottle_reports = reports_renderer(@students, BottleReport).call
