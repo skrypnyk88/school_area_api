717b4a0f93f1264c58ee8e87ec36c2187ff1aa33
dmytro-skrypnyk <skrypnyk_d@ukr.net>
2017-05-18 10:03:10 +0300

    Update health_reports.rb

--- spec/factories/health_reports.rb
+++ spec/factories/health_reports.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :health_report do
     student
     group
-    day { Faker::Date.backward(100) }
+    day { Faker::Date.today }
     special_care [true, false].sample
     health_note { Faker::Lorem.sentence }
   end
