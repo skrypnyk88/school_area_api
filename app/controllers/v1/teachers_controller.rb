21360df7ce9a7a49e593b4b92d657e5ff4dafdba
Dmytro <skrypnyk_d@ukr.net>
2017-05-19 11:13:01 +0300

    LVRUBYM-353: Added upload action

--- app/controllers/v1/teachers_controller.rb
+++ app/controllers/v1/teachers_controller.rb
@@ -2,8 +2,6 @@ module V1
   class TeachersController < ApplicationController
     before_action :find_teacher
 
-    def show; end
-
     def update
       if @teacher.update_attributes(teacher_params)
         head :no_content
@@ -19,6 +17,16 @@ module V1
                                       :email,
                                       :phone,
                                       :locale)
+    end
+
+    def upload
+      attachment = attachment_uploader.call(attachment_owner: @teacher)
+      if attachment.valid?
+        render @teacher, status: :created
+      else
+        render json: { errors: attachment.errors.full_messages },
+               status: :bad_request
+      end
     end
 
     private
